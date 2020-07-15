15755ab63e39e051abbf64d2003d81ffba1cedb8
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-15 18:09:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ def introduction_with_language(name, language)
   
   
 def introduction_with_language_optional(name, language == "Ruby")
-  puts 
+  puts "hello #{name} and #{language}"
 end
 
   
